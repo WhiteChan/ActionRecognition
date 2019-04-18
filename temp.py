73a7c603fd2cdc6373f302482387d76a879dae45
White Chan <420931016@qq.com>
2019-04-18 14:52:49 +0800

    c3d train model

--- temp.py
+++ temp.py
@@ -86,7 +86,7 @@ train_label = np.array(train_label)
 train_label_OneHot = np_utils.to_categorical(train_label, num_classes=101)
 
 model = Sequential()
-model.add(Conv3D(filters=4, kernel_size=[3, 3, 3], input_shape=(100, 240, 320, 3), activation='relu'))
+model.add(Conv3D(filters=2, kernel_size=[3, 3, 3], input_shape=(100, 240, 320, 3), activation='relu'))
 model.add(Flatten())
 model.add(Dense(units=101, activation='softmax'))
 
